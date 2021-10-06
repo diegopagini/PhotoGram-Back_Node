4396186115c7b79b9f6c5b277d17ab1cb2364bc9
Diego Pagini <diegopaginidev@gmail.com>
2021-10-06 08:56:22 +0200

    servidor

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
 /** @format */
-console.log('Hola@');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = __importDefault(require("./classes/server"));
+const server = new server_1.default();
+// Levantar Express
+server.start(() => {
+    console.log(`Servidor corriendo en puerto: ${server.port}`);
+});
--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
 /** @format */
 
-console.log('Hola@');
+import Server from './classes/server';
+
+const server = new Server();
+
+// Levantar Express
+server.start(() => {
+	console.log(`Servidor corriendo en puerto: ${server.port}`);
+});
